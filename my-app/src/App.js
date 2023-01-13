dc3e5b899fdd3d0fb02f2b9ce97a9ba1ebab2fa5
yeahzizi <newyeahzizi@gmail.com>
2023-01-13 17:08:12 +0900

    컴포넌트 반복까지

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-import MyComponent from './MyComponent'
+// import EventPractice from "./EventPractice";
+import IterationSample from "./IterationSample";
+// import MyComponent from "./MyComponent";
+// import Say from "./Say";
 
 const App = () => {
   return (
-    <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
+    // <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
+    <IterationSample />
   );
-}
+};
 
 export default App;
