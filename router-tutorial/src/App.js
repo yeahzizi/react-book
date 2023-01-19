9192e24218a2769ead6ef0348d5913c3cfffd085
yeahzizi <newyeahzizi@gmail.com>
2023-01-19 21:49:35 +0900

    13강 리액트-라우터 까지

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,17 +1,54 @@
 // import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { Route } from 'react-router-dom'
-import About from './About'
-import Home from './Home'
+import "./App.css";
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+// import Profile from "./Profile";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 const App = () => {
   return (
     <div>
-      <Route path='/' component={Home} exact={true}/>
-      <Route path={['/about', '/info']} component={About}/>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        {/* <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li> */}
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "/info"]} component={About} />
+        {/* <Route path="/profile/:username" component={Profile} /> */}
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
-  )
-}
+  );
+};
 
 export default App;
